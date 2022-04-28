440f7a120986bde90aa020e235ca322d8450e32a
LJosueCM <ljosuecm@gmail.com>
2022-04-28 00:43:01 -0500

    5) Prueba de todos los requerimientos instanciadas

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,22 @@
+const Reader = require("../../app/utils/reader")
+const ExplorerService = require("../../app/services/ExplorersService");
+
+describe("Tests para requerimientos", () => {
+    test("1) Consulta todos los estudiantes", () => {
+        const explorers = Reader.readJsonFile("visualpartners.json");
+        const datosEstudiantes = ExplorerService.readAllInfo(explorers)
+        expect(datosEstudiantes).not.toBeNull();
+    });
+
+    test("2) Obtiene emails de todos los estudiantes con certificacion", () => {
+        const explorers = Reader.readJsonFile("visualpartners.json");
+        const emailsEstudiantes = ExplorerService.getEmailEstudiantes(explorers)
+        expect(emailsEstudiantes).not.toBeNull();
+    });
+
+    test("3) Estudiantes con credits mayores a 500", () => {
+        const explorers = Reader.readJsonFile("visualpartners.json");
+        const listaEstudiantesCredits = ExplorerService.getEstudiantesCreditos(explorers)
+        expect(listaEstudiantesCredits).not.toBeNull();
+    });
+});
