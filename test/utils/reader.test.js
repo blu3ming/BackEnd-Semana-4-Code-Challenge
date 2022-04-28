1604e74445ade5abc602da0a58dbd50e8acdac65
LJosueCM <ljosuecm@gmail.com>
2022-04-28 00:26:50 -0500

    4) Prueba de lectura de DB exitosa

--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -2,7 +2,7 @@ const Reader = require("./../../app/utils/reader")
 
 describe("Test para Reader", () => {
     test("1) Lectura de archivo correcta", () => {
-        const explorers = Reader.readJsonFile("visualpartners.com");
+        const explorers = Reader.readJsonFile("visualpartners.json");
         expect(explorers).not.toBeNull();
     });
 });
