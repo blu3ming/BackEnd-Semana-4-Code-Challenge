c1046606226d7d220516e9a31333889fcb1ee273
LJosueCM <ljosuecm@gmail.com>
2022-05-03 12:50:19 -0500

    Pruebas del controller exitosas

--- app/controllers/ExplorersController.js
+++ app/controllers/ExplorersController.js
@@ -1,12 +1,17 @@
+const ExplorersService = require('../services/ExplorersService')
+
 class ExplorersController{
     static readInfoStudents(){
-
+        const estudiantes = ExplorersService.readAllInfo();
+        return estudiantes;
     }
     static getStudentsMail(){
-
+        const emails = ExplorersService.getEmailEstudiantes();
+        return emails;
     }
     static getStudentsWithCredits(){
-        
+        const estudiantes = ExplorersService.getEstudiantesCreditos();
+        return estudiantes;
     }
 }
 
